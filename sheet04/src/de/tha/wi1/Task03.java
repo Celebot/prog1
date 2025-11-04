0226f8b070d11319367c00d0f1919a51ad47ee4d
Cele <Leon_M_Bayer@web.de>
2025-11-04 16:40:15 +0100

    lol

---
+++ sheet04/src/de/tha/wi1/Task03.java
@@ -0,0 +1,10 @@
+package de.tha.wi1;
+
+public class Task03 {
+
+	public static void main(String[] args) {
+	
+		
+	}
+
+}
---
+++ sheet04/src/de/tha/wi1/Task04.java
@@ -0,0 +1,9 @@
+package de.tha.wi1;
+
+public class Task04 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
