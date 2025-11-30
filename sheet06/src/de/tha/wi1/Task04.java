65f87903f4f10fe6dd2a6c7695071ebe72fdd9ed
Celebot <leonloewe3107@web.de>
2025-11-30 10:12:36 +0100

    jzaber

--- sheet06/src/de/tha/wi1/Task04.java
+++ sheet06/src/de/tha/wi1/Task04.java
@@ -2,6 +2,22 @@ package de.tha.wi1;
 
 import static de.tha.tools.Window.*;
 
+import de.tha.tools.Window;
+
+
+
+
+
 public class Task04 {
 	
+	
+	public static int drawCheckBoard(int col,int row) {
+		
+		init
+		
+		
+		
+		
+		return 0;
+	}
 }
